f511530b5a79d586365ff378a2ffdce5f66fe287
Dawid Cieszynski <dawid.cieszynski@crystalcode.pl>
2018-01-13 19:25:58 +0100

    Solution with application and test projects

---
+++ TestableApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestableApplication
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("OK");
+            Console.ReadKey();
+        }
+    }
+}
