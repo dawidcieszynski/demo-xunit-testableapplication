2df3ef87324ef71d3e1e5f1df42f185585c04ed7
Dawid Cieszynski <dawid.cieszynski@crystalcode.pl>
2018-01-18 12:22:01 +0100

    ShouldReadDateFromFileName

---
+++ TestableApplication.Tests/FileNameParserTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace TestableApplication.Tests
+{
+    public class FileNameParserTests
+    {
+        [Theory]
+        [InlineData("2017-01-01.txt", 2017, 1, 1)]
+        [InlineData("2019-10-01.txt", 2019, 10, 1)]
+        public void ShouldReadDateFromFileName(string fileName, int y, int m, int d)
+        {
+            var parser = new FileNameParser();
+
+            var date = parser.Parse(fileName);
+
+            date.Year.Should().Be(y);
+            date.Month.Should().Be(m);
+            date.Day.Should().Be(d);
+        }
+    }
+
+    public class FileNameParser
+    {
+        public DateTime Parse(string fileName)
+        {
+            return DateTime.Parse(fileName.Split('.')[0]);
+        }
+    }
+}
