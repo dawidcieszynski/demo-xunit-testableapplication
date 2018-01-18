77eaca7fc8e3ee6bb5b7f193298ff5ef17a160fa
Dawid Cieszynski <dawid.cieszynski@crystalcode.pl>
2018-01-18 12:56:43 +0100

    FileWriter - ShouldSaveContentToFile - just execution test

---
+++ TestableApplication.Tests/FileWriterTests.cs
@@ -0,0 +1,20 @@
+﻿using System.IO.Abstractions;
+using NSubstitute;
+using Xunit;
+
+namespace TestableApplication.Tests
+{
+    public class FileWriterTests
+    {
+        [Fact]
+        public void ShouldSaveContentToFile()
+        {
+            var fileSystem = Substitute.For<IFileSystem>();
+            var fileWriter = new FileWriter(fileSystem);
+
+            fileWriter.Save("", "");
+
+            fileSystem.File.Received().WriteAllText(Arg.Any<string>(), Arg.Any<string>());
+        }
+    }
+}
---
+++ TestableApplication/FileWriter.cs
@@ -0,0 +1,19 @@
+﻿using System.IO.Abstractions;
+
+namespace TestableApplication
+{
+    public class FileWriter
+    {
+        private readonly IFileSystem _fileSystem;
+
+        public FileWriter(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+        }
+
+        public void Save(string path, string contents)
+        {
+            _fileSystem.File.WriteAllText(path, contents);
+        }
+    }
+}
