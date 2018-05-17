ac11b1f2402c271640a1c0fac67d5f317fde6ab8
Dawid Cieszynski <dawid.cieszynski@crystalcode.pl>
2018-05-17 13:36:11 +0200

    EmailSender - ShouldSendEmailWithContent

---
+++ TestableApplication.Tests/EmailSenderTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using nDumbster.Smtp;
+using Xunit;
+
+namespace TestableApplication.Tests
+{
+    public class EmailSenderTests
+    {
+        [Theory]
+        [AutoData]
+        public void ShouldSendEmailWithContent(string email, string content, string subject)
+        {
+            using (var smtp = SimpleSmtpServer.Start())
+            {
+                var sender = new EmailSender();
+
+                sender.Send($"{email}@example.com", content, subject);
+
+                smtp.ReceivedEmail.First().Subject.Should().Be(subject);
+                smtp.ReceivedEmail.First().Body.Should().Be(content);
+                smtp.ReceivedEmail.First().To.First().Should().Be($"{email}@example.com");
+            }
+        }
+    }
+}
---
+++ TestableApplication/EmailSender.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Mail;
+
+namespace TestableApplication
+{
+    public class EmailSender
+    {
+        public void Send(string email, string content, string subject)
+        {
+            using (var client = new SmtpClient())
+            {
+                var smtpEmail = ConfigurationManager.AppSettings["SmtpUser"];
+                var smtpHost = ConfigurationManager.AppSettings["SmtpHost"];
+                var smtpPort = Convert.ToInt32(ConfigurationManager.AppSettings["SmtpPort"]);
+                var smtpPassword = ConfigurationManager.AppSettings["SmtpPassword"];
+
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Host = smtpHost;
+                client.Port = smtpPort;
+                client.Credentials = new System.Net.NetworkCredential(smtpEmail, smtpPassword);
+
+                using (var mail = new MailMessage(smtpEmail, email))
+                {
+                    mail.Subject = subject;
+                    mail.Body = content;
+
+                    client.Send(mail);
+                }
+            }
+        }
+    }
+}
